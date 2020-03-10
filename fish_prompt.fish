146089b6f477963b056a477b355809007c6706be
Frederick Henderson <frederickjh@henderson-meier.org>
2020-03-10 20:25:08 +0100

    Uncommented the settings for the git prompt to make the branch yellow and show color hints.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -94,9 +94,9 @@ set -g __fish_git_prompt_showdirtystate 'yes'
 set -g __fish_git_prompt_showstashstate 'yes'
 set -g __fish_git_prompt_showupstream 'yes'
 set -g __fish_git_prompt_showuntrackedfiles 'yes'
-#set -g __fish_git_prompt_showcolorhints 'yes'
+set -g __fish_git_prompt_showcolorhints 'yes'
 set -g __fish_git_prompt_show_informative_status 'yes'
-#set -g __fish_git_prompt_color_branch yellow
+set -g __fish_git_prompt_color_branch yellow
 # Status Chars
 set -g __fish_git_prompt_char_dirtystate '⚡'
 set -g __fish_git_prompt_char_stagedstate '→'
